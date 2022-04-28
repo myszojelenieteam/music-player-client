8fcbfb573f6ddd07e3ec844888d10cf3c4b4910b
Kamil Długajczyk <kam.dlugajczyk@gmail.com>
2022-04-29 01:28:56 +0200

    Add getting audio format from wav file (#3)

---
+++ app/src/main/java/eu/myszojelenie/music/player/client/utils/Functions.java
@@ -0,0 +1,15 @@
+package eu.myszojelenie.music.player.client.utils;
+
+import java.io.*;
+import java.util.Base64;
+
+public class Functions {
+    /** Write the object to a Base64 string. */
+    public static String toString( Serializable o ) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream( baos );
+        oos.writeObject( o );
+        oos.close();
+        return Base64.getEncoder().encodeToString(baos.toByteArray());
+    }
+}
